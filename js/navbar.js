e498b2ba37b55548f9ed8c469f94c978e70f84c2
LiroSiza <cesarreyestorres005@gmail.com>
2024-11-14 08:53:53 -0600

    async function

--- js/navbar.js
+++ js/navbar.js
@@ -7,3 +7,29 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+async function getData() {
+    const url = "../API/travel_recommendation_api.json";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+function searchPlace(){
+    const searchInput = document.getElementById("search-input").textContent.toLowerCase();
+    if(searchInput.includes("beach")){ // Verify if the input contains the word "beach"
+        getData(); // Get data from JSON
+    }else if(searchInput.includes("temple")){ // Verify if the input contains the word "temple"
+        getData(); // Get data from JSON
+    }else if(searchInput.includes("country")){ // Verify if the country
+        getData(); // Get data from JSON
+    }
+  }
